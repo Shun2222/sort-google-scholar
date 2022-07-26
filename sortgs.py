947a522d3e2511e2e8cd045e891a45c62af6c949
shun2222 <messishunsuke@gmail.com>
2022-07-26 14:24:41 +0900

    change: output file name

--- sortgs.py
+++ sortgs.py
@@ -293,7 +293,7 @@ def main():
 
     # Save results
     if save_database:
-        fpath_csv = os.path.join("results", path,keyword.replace(' ','_')+"_"+startyear+"-"+endyear+'.csv')
+        fpath_csv = os.path.join("results", path,keyword.replace(' ','_')+"_"+start_year+"-"+end_year+'.csv')
         fpath_csv = fpath_csv[:MAX_CSV_FNAME]
         data_ranked.to_csv(fpath_csv, encoding='utf-8')
 
